c5908276a4f4992e4b3c814540f94af1a64c5185
dousby <dean.ousby@googlemail.com>
2015-08-16 08:13:37 +0100

    Update PBS_Sliding_Window_Plot.R

--- PBS_Calculation/PBS_Sliding_Window_Plot.R
+++ PBS_Calculation/PBS_Sliding_Window_Plot.R
@@ -12,8 +12,6 @@ Pop1.Pop2.Fst <- read.table(Pop1.Pop2.Fst, fill=TRUE)
 Pop1.Pop3.Fst <- read.table(Pop1.Pop3.Fst, fill=TRUE)
 Pop2.Pop3.Fst <- read.table(Pop2.Pop3.Fst, fill=TRUE)
 
-print(head(Pop1.Pop2.Fst))
-
 Position <- Pop1.Pop2.Fst$V2
 
 Pop1.Pop2.Fst <- Pop1.Pop2.Fst$V4
@@ -27,16 +25,13 @@ T.Pop2.Pop3 <- -log(1 - Pop2.Pop3.Fst)
 PBS.Pop3 <- (T.Pop1.Pop3 + T.Pop2.Pop3 - T.Pop1.Pop2)/2
 PBS.Pop3[PBS.Pop3<0] <- 0
 PBS.Pos <- cbind(Position,PBS.Pop3)
-print(head(PBS.Pos))
 PBS.Plot <- cbind(as.integer(11), PBS.Pos[,1], PBS.Pos[,2])
 colnames(PBS.Plot) <- c("CHR", "BP", "P")
-print(head(PBS.Plot))
 library(qqman)
 
 manplot <- data.frame(PBS.Plot)
 maxPBS <- round(max(PBS.Plot[,3])+0.05,digits=2)
 
-print(manplot)
 png("~/PBS.Plot.png", units="in", height=8, width=12, res=500)
 manhattan(manplot, ylim=c(0,maxPBS), logp=FALSE, 
           ylab="PBS", main="",cex = 0.7,cex.lab=1.4, cex.main=1.8, 
